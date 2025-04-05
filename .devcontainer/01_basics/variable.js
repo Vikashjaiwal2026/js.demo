9fb6305bc2212670d335d659f5bc6aba3d2db65f
Vikashjaiwal2026 <206227323+Vikashjaiwal2026@users.noreply.github.com>
2025-04-05 04:46:14 +0000

    coversion of dataType

---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,27 @@
+const accountId=12343
+let accountEmail="vilash.kumar@ "
+var password = "5839039"
+accountCity="jaipur"
+
+accountEmail="vikash.jaiswal@"
+password="54983838"
+accountCity="delhi"
+
+// console.log(accountId)
+console.table([accountId,accountEmail,password,accountCity])
+// let score="33abc"
+let score =null
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+let isLoggedIn="vikash"
+ let booleanInLoggedIn=Boolean(isLoggedIn)
+ console.log(booleanInLoggedIn)
+
+ //1=>true;0=>false
+ // ""=>false
+ //"hitesh"=>true
+ let someNumber=33
+ let stringNumber=String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
